9dea428f0be0cdcdf48ebc3c8b3c1d62c55d6873
Weston Wolfcale <wolfcaleweston@gmail.com>
2016-11-23 21:37:34 -0500

    Added login/logout functionality as well as ability to edit/remove based on login or not.

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import include, url, patterns
+from django.contrib.auth import views
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
-]
+)
