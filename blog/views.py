2bd0067592fe7c1a7fd3d3e359c038f19b9c5849
Weston Wolfcale <wolfcaleweston@gmail.com>
2016-11-23 21:53:56 -0500

    Added ability to comment on posts.

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 
 # Create your views here.
 def post_list(request):
@@ -62,3 +62,16 @@ def post_remove(request, pk):
 def logout(request):
 	logout(request)
 	return redirect('logout')
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
